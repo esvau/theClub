526cffbe039f930882c4890d4716862f209cf08f
Sebastian Voss <voss@SebastianFORTISSMac.local>
2014-11-18 17:28:50 +0100

    initial

---
+++ theClub/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  theClub
+//
+//  Created by Sebastian Voss on 18.11.14.
+//  Copyright (c) 2014 qwitto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
